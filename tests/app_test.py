47215947014f5759ff66d72a047f722dbfc2baa4
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-21 17:31:01 +0200

    test for existing db

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,5 @@
 from project.app import app
+from pathlib import Path
 
 def test_index():
     tester = app.test_client()
@@ -6,3 +7,6 @@ def test_index():
 
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+def test_database():
+    assert Path("flaskr.db").is_file()
